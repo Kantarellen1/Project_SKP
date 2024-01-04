e33250e25c7dea849c9ca88906e9b93ba92efb95
Kasper Korthsen <Kalletheman@live.dk>
2024-01-04 10:59:50 +0100

    Added servo motor control

--- Project_SKP.ino
+++ Project_SKP.ino
@@ -1,3 +1,6 @@
+#include <Servo.h>
+
+#include <Servo.h>
 /*Project 2 
 int switchstate = 0;
 
@@ -30,7 +33,7 @@ void loop() {
     delay(250);
   }
 }*/
-
+/*
 const int greenLEDPin = 9;
 const int redLEDPin = 10;
 const int blueLEDPin = 11;
@@ -84,4 +87,37 @@ void loop() {
   analogWrite(redLEDPin, redValue);
   analogWrite(greenLEDPin, greenValue);
   analogWrite(blueLEDPin, blueValue);
+}*/
+
+#include <Servo.h>
+ 
+Servo myServo;  // create a servo object 
+ 
+int const potPin = A0; // analog pin used to connect the potentiometer
+int potVal;  // variable to read the value from the analog pin 
+int angle;   // variable to hold the angle for the servo motor 
+ 
+void setup() {
+  myServo.attach(9); // attaches the servo on pin 9 to the servo object 
+  Serial.begin(9600); // open a serial connection to your computer
+}
+ 
+void loop() {
+  potVal = analogRead(potPin); // read the value of the potentiometer
+  // print out the value to the serial monitor
+  Serial.print("potVal: ");
+  Serial.print(potVal);
+ 
+  // scale the numbers from the pot 
+  angle = map(potVal, 0, 1023, 0, 179);
+ 
+  // print out the angle for the servo motor 
+  Serial.print(", angle: ");
+  Serial.println(angle); 
+ 
+  // set the servo position  
+  myServo.write(angle);
+ 
+  // wait for the servo to get there 
+  delay(15);
 }
